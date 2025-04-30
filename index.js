ff4e7338c229dc62f0bed86a25782649a5eea10c
Younis <younisniit@gmail.com>
2025-04-30 10:09:02 +0530

    #removed unnecessary Files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { craeteRoot, createRoot } from 'react-dom/client';
-import App from './App';
 import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App/>);
